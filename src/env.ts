d8df41d101e5be0437e9b0c4d12d1fb784b5d887
Max Kammler <max.kammler@icloud.com>
2023-11-07 10:08:54 +0100

    add option to increase token size; closes #246

--- src/env.ts
+++ src/env.ts
@@ -38,6 +38,8 @@ export const {
   CHATGPT_IGNORE_MEDIA,
   CHATGPT_REVERSE_PROXY,
   CHATGPT_TEMPERATURE,
+  CHATGPT_MAX_CONTEXT_TOKENS,
+  CHATGPT_MAX_PROMPT_TOKENS,
 } = parseEnv(process.env, {
   DATA_PATH:                   { schema: z.string().default("./storage"),          description: "Set to /storage/ if using docker, ./storage if running without" },
   KEYV_BACKEND:                { schema: z.enum(["file", "other"]).default("file"),description: "Set the Keyv backend to 'file' or 'other' if other set KEYV_URL" },
@@ -72,5 +74,7 @@ export const {
   CHATGPT_PROMPT_PREFIX:       { schema: z.string().default('Instructions:\nYou are ChatGPT, a large language model trained by OpenAI.'), description: "Instructions to feed to ChatGPT on startup"},
   CHATGPT_IGNORE_MEDIA:        { schema: z.boolean().default(false),               description: "Wether or not the bot should react to non-text messages"},
   CHATGPT_REVERSE_PROXY:       { schema: z.string().default(""),                   description: "Change the api url to use another (OpenAI-compatible) API endpoint" },
-  CHATGPT_TEMPERATURE:         { schema: z.number().default(0.8),                  description: "Set the temperature for the model" }
+  CHATGPT_TEMPERATURE:         { schema: z.number().default(0.8),                  description: "Set the temperature for the model" },
+  CHATGPT_MAX_CONTEXT_TOKENS:  { schema: z.number().default(4097), description: "Davinci models have a max context length of 4097 tokens, but you may need to change this for other models." }
+  CHATGPT_MAX_PROMPT_TOKENS    { schema: z.number().default(3097), description: "You might want to lower this to save money if using a paid model. Earlier messages will be dropped until the prompt is within the limit." }
 });
